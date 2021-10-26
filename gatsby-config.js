98cd716e4ac3a8b0762fe239810c809a9b2fe07a
pruanki <prudvihemanth@gmail.com>
2021-10-26 18:06:10 +0530

    added TS support, jest, cypress, eslint and SSR & DS configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,12 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "redcrackle-gatsby-starter",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-typescript",
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
+    plugins: [
+      {
+        resolve: `gatsby-plugin-typescript`,
+        options: {
+          isTSX: true, // defaults to false
+          jsxPragma: `jsx`, // defaults to "React"
+          allExtensions: true, // defaults to false
+        },
       },
-    },
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        pathToEmotionCacheProps: `src/emotion-cache-props`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+    ],
+  }
